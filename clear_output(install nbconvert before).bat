9b127fedccba9be3fd3d48f8662129eb3e7a9438
DESKTOP-9QA13RB\yunsh <kcng0@cse.cuhk.edu.hk>
2023-10-26 00:21:19 +0100

    feat: eliminate possibility of song classification

--- clear_output(install nbconvert before).bat
+++ clear_output(install nbconvert before).bat
@@ -1,22 +1,22 @@
 @echo off
 
 rem Check if nbconvert is installed
-jupyter nbconvert --version >nul 2>nul
-if %errorlevel%==0 (
-    echo nbconvert is already installed.
-    set /p "continue=Do you want to run nbconvert? (Y/N): "
-    if /i "%continue%"=="Y" (
-        jupyter nbconvert --ClearOutputPreprocessor.enabled=True --clear-output *.ipynb
-    ) else (
-        echo nbconvert will not be executed.
+jupyter nbconvert --version >nul 2>nul && (
+    echo nbconvert is installed.
+    echo Cleaning all notebooks...
+    jupyter nbconvert --ClearOutputPreprocessor.enabled=True --clear-output *.ipynb && (
+        echo Cleaned all notebooks successfully.
+        exit /b 0
+    ) || (
+        echo Failed to clean notebooks. Please try to install nbconvert manually.
     )
-) else (
+) || (
     echo nbconvert is not installed. Do you want to install it? (Y/N)
     set /p "install=Do you want to install nbconvert? (Y/N): "
     if /i "%install%"=="Y" (
         pip install nbconvert
         rem Check if nbconvert is installed
-        jupyter nbconvert --clear-output --inplace *.ipynb && (
+        jupyter nbconvert --ClearOutputPreprocessor.enabled=True --clear-output *.ipynb && (
             echo Cleaned all notebooks successfully.
         ) || (
             echo Failed to clean notebooks. Please try to install nbconvert manually.
